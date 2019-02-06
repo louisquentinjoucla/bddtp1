43abc799e752ca9797d16a24a848b4442e7c4a83
louisquentinjoucla <louis-quentin.joucla@isen.yncrea.fr>
2019-02-06 16:02:05 -0500

    Exercice 2 (pagerank)

--- PageRank.scala
+++ PageRank.scala
@@ -3,13 +3,14 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.{SQLContext, SparkSession}
 import scala.collection.mutable.WrappedArray
 
-case class Page(pageRank: Double, adjlist:Array[Int]);
+case class Page(name: String, pageRank: Double, adjlist:Array[String]);
 
 object PageRank {
 
   def main(args: Array[String]): Unit = {
 
-    val MAX_ITER_PR = 1;
+    val MAX_ITER_PR: Int = 20;
+    val DAMPING : Double = 0.85;
 
 	// local[k] launch spark local & limit threads numbers
 	val conf: SparkConf = new SparkConf()
@@ -17,35 +18,50 @@ object PageRank {
 							.setMaster("local[2]")
 
 	val ss: SparkSession = SparkSession
-									.builder()
-									.config(conf)
-									.getOrCreate();
+                            .builder()
+                            .config(conf)
+                            .getOrCreate();
 	val sc: SparkContext = ss.sparkContext
 
 	// Reduce the amount of log
 	ss.sparkContext.setLogLevel("ERROR")
 
     // Creating the graph
-    var pageA : Page = new Page(0, Array(1, 2));
-    var pageB : Page = new Page(0, Array(2));
-    var pageC : Page = new Page(0, Array(0));
-    var pageD : Page = new Page(0, Array(2));
+    var pageA : Page = new Page("A", 1d, Array("B", "C"));
+    var pageB : Page = new Page("B", 1d, Array("C"));
+    var pageC : Page = new Page("C", 1d, Array("A"));
+    var pageD : Page = new Page("D", 1d, Array("C"));
     var graph : Array[Page] = Array(pageA, pageB, pageC, pageD);
 
     // Putting the graph in a RDD
     val rddGraph = sc.parallelize(graph);
-    rddGraph.foreach(println);
 
     // PageRank
     var i = 0;
+    var ranks = rddGraph.map((page) => {
+           (page.name, 1d);
+    });
+
+    var array = rddGraph.map((page) => {
+           (page.name, page.adjlist);
+    });
+
+    var pages = rddGraph.map((page) => {
+           (page.name, 0d);
+    });
+
 
     for(i <- 0 until MAX_ITER_PR) {
-        rddGraph.foreach((page) => {
-            println(page);
-        });
+        println("Iteration " + (i+1));
+        var outlinks = ranks.join(array).values.flatMap{case (rank,array) =>
+            array.map(url => (url, rank / array.size))
+        };
+
+        var contribs = pages.union(outlinks);
+
+        ranks = contribs.reduceByKey(_ + _).map{case(url, pr) => (url, (1-DAMPING) + DAMPING * pr)};
+        ranks.foreach(println)
     }
-
-
 	sc.stop
   }
 }
